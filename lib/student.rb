f34ec1c80cfb1424e2dd8af72927aa3b69ba0154
David Strauch <dmaster18@gmail.com>
2019-10-25 00:04:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
   def self.find_by_name(name)
     sql = "SELECT*FROM students WHERE name = ? LIMIT 1"
     DB[:conn].execute(sql, name).map do |row|
-      
+      student = 
     end
   end
 
